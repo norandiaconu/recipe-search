79c9da8e47b6c40fa2798f29566157e9c435732b
Noran Diaconu <norandiaconu@gmail.com>
2024-08-28 20:22:48 -0400

    Updated to Angular 17

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,43 +1,43 @@
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
-  let fixture: ComponentFixture<AppComponent>;
-  let app: AppComponent;
-  beforeAll(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+describe('AppComponent', () => {
+    let fixture: ComponentFixture<AppComponent>;
+    let app: AppComponent;
+    beforeAll(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.componentInstance;
+        jest.mock('src/environments/environment', () => ({
+            environment: {
+                production: false,
+                apiKey: '123'
+            }
+        }));
     });
-    fixture = TestBed.createComponent(AppComponent);
-    app = fixture.componentInstance;
-    jest.mock("src/environments/environment", () => ({
-      environment: {
-        production: false,
-        apiKey: '123'
-      }
-    }));
-  })
 
-  it("should setup app", () => {
-    expect(app).toBeTruthy();
-  });
+    it('should setup app', () => {
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'recipe-search'`, () => {
-    expect(app.title).toEqual('recipe-search');
-  });
+    it("should have as title 'recipe-search'", () => {
+        expect(app.title).toEqual('recipe-search');
+    });
 
-  it(`should display`, () => {
-    const recipe = {
-      name: 'testName',
-      sections: [{name: 'sectionName', components: [{raw_text: 'sectionText'}]}],
-      instructions: [{display_text: 'instructionText'}],
-      slug: 'testSlug'
-    };
-    app.display(recipe);
-    expect(app.recipeName).toEqual('testName');
-    expect(app.ingredients).toEqual([{name: 'sectionName', components: [{raw_text: 'sectionText'}]}]);
-    expect(app.instructions).toEqual([{display_text: 'instructionText'}]);
-    expect(app.slug).toEqual('testSlug');
-  });
+    it('should display', () => {
+        const recipe = {
+            name: 'testName',
+            sections: [{ name: 'sectionName', components: [{ raw_text: 'sectionText' }] }],
+            instructions: [{ display_text: 'instructionText' }],
+            slug: 'testSlug'
+        };
+        app.display(recipe);
+        expect(app.recipeName).toEqual('testName');
+        expect(app.ingredients).toEqual([{ name: 'sectionName', components: [{ raw_text: 'sectionText' }] }]);
+        expect(app.instructions).toEqual([{ display_text: 'instructionText' }]);
+        expect(app.slug).toEqual('testSlug');
+    });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,75 +2,86 @@ import { Component } from '@angular/core';
 import { z } from 'zod';
 import { environment } from 'src/environments/environment';
 
-const Recipe = z.object({
-  name: z.string(),
-  sections: z.array(z.object({
+const recipeZ = z.object({
     name: z.string(),
-    components: z.array(z.object({
-      raw_text: z.string()
-    }))
-  })),
-  instructions: z.array(z.object({
-    display_text: z.string()
-  })),
-  slug: z.string()
+    sections: z.array(
+        z.object({
+            name: z.string(),
+            components: z.array(
+                z.object({
+                    raw_text: z.string()
+                })
+            )
+        })
+    ),
+    instructions: z.array(
+        z.object({
+            display_text: z.string()
+        })
+    ),
+    slug: z.string()
 });
-type Recipe = z.infer<typeof Recipe>;
+type Recipe = z.infer<typeof recipeZ>;
+
+interface Result {
+    count: number;
+    results: Recipe[];
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'recipe-search';
-  recipes: Recipe[] = [];
-  recipeName = "";
-  ingredients: any[] = [];
-  instructions: any[] = [];
-  slug = "";
+    title = 'recipe-search';
+    recipes: Recipe[] = [];
+    recipeName = '';
+    ingredients: any[] = [];
+    instructions: any[] = [];
+    slug = '';
 
-  async searchTasty(ingredient1: string, ingredient2: string, ingredient3: string): Promise<void> {
-    console.log(ingredient1, ingredient2, ingredient3);
-    const ingredientList: string[] = [];
-    ingredientList.push(ingredient1);
-    ingredientList.push(ingredient2);
-    ingredientList.push(ingredient3);
-    let url = `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q=`;
-    ingredientList.forEach(ingredient => {
-      if (ingredient) {
-        url = url + ingredient + ',';
-      }
-    });
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': environment.apiKey,
-        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-      }
-    };
+    async searchTasty(ingredient1: string, ingredient2: string, ingredient3: string): Promise<void> {
+        console.log(ingredient1, ingredient2, ingredient3);
+        const ingredientList: string[] = [];
+        ingredientList.push(ingredient1);
+        ingredientList.push(ingredient2);
+        ingredientList.push(ingredient3);
+        let url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q=';
+        ingredientList.forEach((ingredient) => {
+            if (ingredient) {
+                url = url + ingredient + ',';
+            }
+        });
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': environment.apiKey,
+                'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+            }
+        };
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      this.recipes = result.results;
-      console.log(result.results);
-    } catch (error) {
-      console.error(error);
+        try {
+            const response = await fetch(url, options);
+            const result = (await response.json()) as Result;
+            this.recipes = result.results;
+            console.log(result.results);
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  display(recipe: Recipe): void {
-    console.log(recipe);
-    this.recipeName = recipe.name;
-    this.ingredients = recipe.sections;
-    this.instructions = recipe.instructions;
-    this.slug = recipe.slug;
-    setTimeout(() => {
-      window.scrollTo({
-        top: 500,
-        behavior: "smooth"
-      });
-    }, 100);
-  }
+    display(recipe: Recipe): void {
+        console.log(recipe);
+        this.recipeName = recipe.name;
+        this.ingredients = recipe.sections;
+        this.instructions = recipe.instructions;
+        this.slug = recipe.slug;
+        setTimeout(() => {
+            window.scrollTo({
+                top: 500,
+                behavior: 'smooth'
+            });
+        }, 100);
+    }
 }
