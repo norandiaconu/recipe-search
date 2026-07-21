233f509eacaf42dfb9693dd1c5661009d09b36bd
Noran Diaconu <norandiaconu@gmail.com>
2026-07-21 19:17:43 -0400

    Updated linting

--- obfuscate.js
+++ obfuscate.js
@@ -1,13 +1,13 @@
-var fs = require('fs');
+import fs from "fs";
 
-fs.readFile('./docs/browser/index.html', 'utf8', (err, htmlString) => {
+fs.readFile("./docs/browser/index.html", "utf8", (err, htmlString) => {
     const replaced = htmlString
         .replace(/"main\./, '"main-obfuscated.')
         .replace(/"polyfills\./, '"polyfills-obfuscated.')
         .replace(/"runtime\./, '"runtime-obfuscated.');
-    fs.writeFileSync('./docs/browser/index.html', replaced);
-    fs.readdirSync('./docs/browser')
+    fs.writeFileSync("./docs/browser/index.html", replaced);
+    fs.readdirSync("./docs/browser")
         .filter((f) => /main\..*\.js|polyfills\..*\.js|runtime\..*\.js/.test(f))
-        .map((f) => fs.unlinkSync('./docs/browser/' + f));
-    console.log('Completed replacing files in /docs/browser');
+        .map((f) => fs.unlinkSync("./docs/browser/" + f));
+    console.log("Completed replacing files in /docs/browser");
 });
